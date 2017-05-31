b7eb755b2c5a832d8eb3c36d83ed14550f280be0
npl22 <nli22@jhu.edu>
2017-05-31 09:59:22 -0700

    Remove Pong header so that canvas is fullscreen

--- js/script.js
+++ js/script.js
@@ -13,9 +13,49 @@ function handleResize() {
   // const scale = w/100;
 }
 
-c.beginPath();
-c.arc(300, 300, 30, 0, 2*Math.PI, false);
-c.fill();
+let paddleY = 100;
+let x = 300;
+let y = 300;
+let dx = 10;
+let dy = 10;
+const radius = 30;
+function animate() {
+  window.requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  c.fillStyle = 'black';
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  c.fillStyle = 'white';
 
-c.fillRect(10, 300, 70, 225);
-c.fillRect(1200, 100, 70, 225);
+  c.fillRect(10, 300, 70, 225);
+  c.fillRect(800, paddleY, 70, 225);
+
+  c.beginPath();
+  c.arc(x, y, radius, 0, 2*Math.PI, false);
+  c.fill();
+
+  x += dx;
+  y += dy;
+
+  if (x + radius >= 800 || x - radius <= 80) {
+    dx = -dx;
+  }
+
+  if (y + radius >= window.innerHeight || y - radius <= 0) {
+    dy = -dy;
+  }
+}
+
+window.addEventListener('keydown', e => {
+  switch(e.key) {
+    case("ArrowDown"):
+      paddleY -= 8;
+      break;
+    case("ArrowUp"):
+      paddleY += 8;
+      break;
+    default:
+      return;
+  }
+});
+
+animate();
